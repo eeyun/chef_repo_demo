9b23c0b804f8b02d9901a40aef61b9fc70725eb9
Stephen Lauck <stephen.lauck@gmail.com>
2015-08-27 10:41:32 -0400

    rename policy update policy and create lock

--- chef-server.rb
+++ chef-server.rb
@@ -4,15 +4,14 @@
 # https://github.com/opscode/chef-dk/blob/master/POLICYFILE_README.md
 
 # A name that describes what the system you're building with Chef does.
-name "Chef_Server"
+name "chef-server"
 
 # Where to find external cookbooks:
 default_source :community
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list "cookbook::recipe"
+run_list "chef-server::default"
 
 # Specify a custom source for a single cookbook:
 # cookbook "development_cookbook", path: "../cookbooks/development_cookbook"
-cookbook 'git'
-cookbook 'apt'
+cookbook 'chef-server'
