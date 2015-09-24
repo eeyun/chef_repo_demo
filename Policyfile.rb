1fce3ff8024dbbbb03ddf1dc06050e02d775d6b3
Stephen Lauck <stephen.lauck@gmail.com>
2015-09-24 11:29:39 -0400

    update Policyfile and Policyfile.lock
    add pipeline
    add supermarket

--- Policyfile.rb
+++ Policyfile.rb
@@ -1,17 +1,22 @@
 # A name that describes what the system you're building with Chef does.
-name 'chef-server'
+name 'supermarket'
 
 # Where to find external cookbooks:
 default_source :community
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list(
-  'git'
+run_list( 
+  "supermarket-omnibus-cookbook"
 )
 
 # Specify a custom source for a single cookbook:
 # cookbook "development_cookbook", path: "../cookbooks/development_cookbook"
 
+# community cookbooks
 cookbook 'git'
 cookbook 'apt'
 cookbook 'jenkins'
+cookbook 'supermarket-omnibus-cookbook'
+
+# repo cookbooks
+cookbook 'pipeline', git: 'https://github.com/chef-solutions/pipeline.git'
